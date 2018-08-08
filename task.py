dae5e8d37ea6c2461ec00f9195c0b7f0b0ca8b63
Lim Swee Kiat <limsweekiat@gmail.com>
2018-08-08 17:04:17 +0800

    Minor edit to task

--- task.py
+++ task.py
@@ -12,7 +12,7 @@ class LineTask():
 		self.uncropped_width = 50
 
 	def next_batch(self, batchsize):
-		angles = np.random.rand(batchsize) * (self.angle_range[1] - self.angle_range[0]) + self.angle_range[0]
+		angles = np.random.rand(batchsize * 2) * (self.angle_range[1] - self.angle_range[0]) + self.angle_range[0]
 		images = np.zeros((batchsize, self.uncropped_height, self.uncropped_width))
 		images[:, int(self.uncropped_width / 2):int(self.uncropped_width / 2 + self.length), int(self.uncropped_width / 2)] = 1
 		image = images[0]
@@ -23,11 +23,10 @@ class LineTask():
 			(self.uncropped_height - self.height) / 2 + self.height,
 		)
 		samples_1 = []
-		for angle in angles:
+		for angle in angles[:batchsize]:
 			samples_1.append(np.array(Image.fromarray(image).rotate(angle).crop(crop_dim)))
 		samples_2 = []
-		for angle in angles:
-			angle += 180
+		for angle in angles[batchsize:]:
 			samples_2.append(np.array(Image.fromarray(image).rotate(angle).crop(crop_dim)))
 		alpha = np.random.rand(batchsize) * 0.5
 		return np.array(samples_1).reshape(-1, self.height, self.width, 1), np.array(samples_2).reshape(-1, self.height, self.width, 1), alpha
