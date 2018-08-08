9fd36e4fe80771da6cafa1fad9c310fbe0a376cd
Lim Swee Kiat <limsweekiat@gmail.com>
2018-08-08 16:58:59 +0800

    Updated for Line Task

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 import numpy as np
 from absl import flags
 from absl import app
+from PIL import Image
 
 from models import ACAI
 from task import LineTask
@@ -10,21 +11,58 @@ from task import LineTask
 FLAGS = flags.FLAGS
 
 flags.DEFINE_bool('train', False, 'Train')
+flags.DEFINE_bool('generate', False, 'Generate')
+
+flags.DEFINE_integer('steps', 5000, 'Number of training steps')
+flags.DEFINE_string('savepath', 'saved_models/', 'Path to save or load models')
 
 def main(unused_args):
-	model = ACAI()
-	task = LineTask()
-	for i in range(100):
-		samples_1, samples_2, alpha = task.next_batch(64)
-		feed_dict = {
-			model.inputs_image_1: samples_1,
-			model.inputs_image_2: samples_2,
-			model.inputs_alpha: alpha,
-		}
-		autoencoder_loss, _ = sess.run([model.autoencoder_loss, autoencoder_optimize], feed_dict)
-		critic_loss, _ = sess.run([model.critic_loss, critic_optimize], feed_dict)
-		if (i + 1) % 50 == 0:
-			print('Step {} - autoencoder_loss: {:.3f} - critic_loss: {:.3f}'.format(i + 1, autoencoder_loss, critic_loss))
+
+	if FLAGS.train:
+		model = ACAI()
+		task = LineTask()
+		sess = tf.Session()
+		sess.run(tf.global_variables_initializer())
+		min_autoencoder_loss = np.inf
+		for step in range(1, FLAGS.steps + 1):
+			samples_1, samples_2, alpha = task.next_batch(64)
+			feed_dict = {
+				model.inputs_image_1: samples_1,
+				model.inputs_image_2: samples_2,
+				model.inputs_alpha: alpha,
+			}
+			autoencoder_loss, recon_loss_1, recon_loss_2, _, recon = sess.run([model.autoencoder_loss, model.reconstruction_loss_1, model.reconstruction_loss_2, model.autoencoder_optimize, model.decoder_1.outputs], feed_dict)
+			critic_loss, _ = sess.run([model.critic_loss, model.critic_optimize], feed_dict)
+			critic_score = sess.run(model.critic_interpolation.outputs, feed_dict)
+			if step % 50 == 0:
+				print('Step {} - autoencoder loss: {:.3f} - critic loss: {:.3f} - average critic score: {:.3f}'.format(step, autoencoder_loss, critic_loss, np.mean(critic_score)))
+				print((np.max(recon), np.min(recon)))
+				print(recon_loss_1)
+				print(recon_loss_2)
+				model.save(sess, FLAGS.savepath, step)
+	elif FLAGS.generate:
+		model = ACAI()
+		task = LineTask()
+		sess = tf.Session()
+		sess.run(tf.global_variables_initializer())
+		model.load(sess, FLAGS.savepath, verbose=True)
+		samples_1, samples_2, _ = task.next_batch(1)
+		intervals = 10
+		interpolations = []
+
+		for i in range(intervals + 1):
+			alpha = 1 - i / intervals
+			feed_dict = {
+				model.inputs_image_1: samples_1,
+				model.inputs_image_2: samples_2,
+				model.inputs_alpha: [alpha],
+			}
+			interpolation = sess.run(model.decoder_interpolation.outputs, feed_dict).reshape(32, 32)
+			interpolations.append(interpolation)
+		samples = np.concatenate([samples_1[0].reshape(32, 32)] + interpolations + [samples_2[0].reshape(32, 32)], axis=1)
+		scale = 2
+		Image.fromarray(samples * 255).resize((samples.shape[1] * scale, samples.shape[0] * scale)).show()
+
 
 if __name__ == '__main__':
 	app.run(main)
--- models.py
+++ models.py
@@ -5,7 +5,9 @@ from BaseModel import BaseModel
 
 class ACAI(BaseModel):
 
-	def __init__(self, name='acai'):
+	def __init__(self, name='acai', gamma=0.2, lmbda=0.5):
+		self.gamma = gamma
+		self.lmbda = lmbda
 		self.name = name
 		with tf.variable_scope(self.name):
 			self.build_model()
@@ -30,39 +32,53 @@ class ACAI(BaseModel):
 			name='inputs_alpha',
 		)
 
-		gamma = 0.2
-		lmbda = 0.5
-
+		# Reconstruction for x_1
 		self.encoder_1 = Encoder(self.inputs_image_1)
-		self.encoder_2 = Encoder(self.inputs_image_2)
-		self.latent_interpolation = self.inputs_alpha * self.encoder_1.outputs + (1 - self.inputs_alpha) * self.encoder_2.outputs
 		self.decoder_1 = Decoder(self.encoder_1.outputs)
-		self.decoder_2 = Decoder(self.encoder_2.outputs)
-		self.decoder_interpolation = Decoder(self.interpolation)
-		self.critic_interpolation = Critic(self.decoder_interpolation.outputs)
-		self.critic_regular = Critic(gamma * self.inputs_image_1 + (1 - gamma) * self.decoder_1.outputs)
+		self.reconstruction_1 = self.decoder_1.outputs
 
-		critic_loss_1 = tf.losses.mean_squared_error(labels=self.inputs_alpha, predictions=self.critic_interpolation.outputs)
-		critic_loss_2 = self.critic_regular ** 2
-		critic_loss = critic_loss_1 + critic_loss_2
+		# Reconstruction for x_2
+		self.encoder_2 = Encoder(self.inputs_image_2)
+		self.decoder_2 = Decoder(self.encoder_2.outputs)
+		self.reconstruction_2 = self.decoder_2.outputs
+
+		# Latent interpolation between x_1 and x_2
+		self.latent_interpolation = tf.reshape(self.inputs_alpha, [-1, 1, 1, 1]) * self.encoder_1.outputs + tf.reshape((1 - self.inputs_alpha), [-1, 1, 1, 1]) * self.encoder_2.outputs
+		self.decoder_interpolation = Decoder(tf.reshape(self.latent_interpolation, [-1, 2, 2, 16]))
+		self.reconstruction_interpolation = self.decoder_interpolation.outputs
 		
-		reconstruction_loss = tf.losses.mean_squared_error(labels=self.inputs_image, predictions=self.decoder.outputs)
-		regularization_loss = lmda * self.critic_interpolation ** 2
-		autoencoder_loss = reconstruction_loss + regularization_loss
+		# Critic scores
+		self.critic_interpolation = Critic(self.reconstruction_interpolation) # This should be alpha
+		self.critic_regular_1 = Critic(self.gamma * self.inputs_image_1 + (1 - self.gamma) * self.reconstruction_1) # This should be 0
+		self.critic_regular_2 = Critic(self.gamma * self.inputs_image_2 + (1 - self.gamma) * self.reconstruction_2) # This should be 0
+		
+
+		# Autoencoder losses
+		self.reconstruction_loss_1 = tf.losses.mean_squared_error(labels=self.inputs_image_1, predictions=self.decoder_1.outputs)
+		self.reconstruction_loss_2 = tf.losses.mean_squared_error(labels=self.inputs_image_2, predictions=self.decoder_2.outputs)
+		regularization_loss = self.lmbda * tf.reduce_mean(self.critic_interpolation.outputs ** 2)
+		self.autoencoder_loss = self.reconstruction_loss_1 + self.reconstruction_loss_2 + regularization_loss
+
+		autoencoder_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'acai/encoder') + tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'acai/decoder')
+		self.autoencoder_optimize = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(self.autoencoder_loss, var_list=autoencoder_vars)
+
+		# Critic losses
+		critic_loss_1 = tf.losses.mean_squared_error(labels=self.inputs_alpha, predictions=self.critic_interpolation.outputs)
+		critic_loss_2 = tf.reduce_mean(self.critic_regular_1.outputs ** 2) + tf.reduce_mean(self.critic_regular_2.outputs ** 2)
+		self.critic_loss = critic_loss_1 + critic_loss_2
 
 		critic_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'acai/critic')
-		critic_optimize = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(critic_loss, var_list=critic_vars)
-		autoencoder_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'acai/encoder') + tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'acai/decoder')
-		autoencoder_optimize = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(critic_loss, var_list=autoencoder_vars)
+		self.critic_optimize = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(self.critic_loss, var_list=critic_vars)
+		
 
 class Encoder(BaseModel):
 
 	def __init__(self, inputs, name='encoder'):
 		self.inputs = inputs
 		self.name = name
-		with tf.variable_scope(self.name):
+		with tf.variable_scope(self.name, reuse=tf.AUTO_REUSE):
 			self.build_model()
-			variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, self.name)
+			variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, '/'.join(['acai', self.name]))
 			self.saver = tf.train.Saver(var_list=variables, max_to_keep=3)
 
 	def build_model(self):
@@ -76,6 +92,7 @@ class Encoder(BaseModel):
 				strides=(1, 1),
 				padding='same',
 				activation=None,
+				# kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
 				name='conv{}_1'.format(i),
 			)
 			relu_1 = tf.nn.leaky_relu(
@@ -89,16 +106,13 @@ class Encoder(BaseModel):
 				strides=(1, 1),
 				padding='same',
 				activation=None,
+				# kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
 				name='conv{}_2'.format(i),
 			)
-			# No Leaky ReLU for last layer
-			if i == len(n_filters) - 1:
-				relu_2 = conv_2
-			else:
-				relu_2 = tf.nn.leaky_relu(
-					features=conv_2,
-					alpha=0.2,
-				)
+			relu_2 = tf.nn.leaky_relu(
+				features=conv_2,
+				alpha=0.2,
+			)
 			pool = tf.layers.average_pooling2d(
 				inputs=relu_2,
 				pool_size=(2, 2),
@@ -106,21 +120,46 @@ class Encoder(BaseModel):
 				padding='valid'
 			)
 			running_output = pool
-		self.outputs = running_output
+
+		final_conv_1 = tf.layers.conv2d(
+			inputs=running_output,
+			filters=16,
+			kernel_size=(3, 3),
+			strides=(1, 1),
+			padding='same',
+			activation=None,
+			# kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+			name='conv_final_1',
+		)
+		final_relu = tf.nn.leaky_relu(
+			features=final_conv_1,
+			alpha=0.2,
+		)
+		final_conv_2 = tf.layers.conv2d(
+			inputs=final_relu,
+			filters=16,
+			kernel_size=(3, 3),
+			strides=(1, 1),
+			padding='same',
+			activation=None,
+			# kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+			name='conv_final_2',
+		)
+		self.outputs = final_conv_2
 
 class Decoder(BaseModel):
 
 	def __init__(self, inputs, name='decoder'):
 		self.inputs = inputs
 		self.name = name
-		with tf.variable_scope(self.name):
+		with tf.variable_scope(self.name, reuse=tf.AUTO_REUSE):
 			self.build_model()
-			variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, self.name)
+			variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, '/'.join(['acai', self.name]))
 			self.saver = tf.train.Saver(var_list=variables, max_to_keep=3)
 
 	def build_model(self):
 		running_output = self.inputs
-		n_filters = [8, 4, 2, 1]
+		n_filters = [16, 8, 4, 2]
 		for i, filters in enumerate(n_filters):
 			conv_1 = tf.layers.conv2d(
 				inputs=running_output,
@@ -129,6 +168,7 @@ class Decoder(BaseModel):
 				strides=(1, 1),
 				padding='same',
 				activation=None,
+				# kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
 				name='conv{}_1'.format(i),
 			)
 			relu_1 = tf.nn.leaky_relu(
@@ -142,9 +182,9 @@ class Decoder(BaseModel):
 				strides=(1, 1),
 				padding='same',
 				activation=None,
+				# kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
 				name='conv{}_2'.format(i),
 			)
-			# No Leaky ReLU for last layer
 			relu_2 = tf.nn.leaky_relu(
 				features=conv_2,
 				alpha=0.2,
@@ -154,7 +194,7 @@ class Decoder(BaseModel):
 				size=(tf.shape(relu_2)[1]*2, tf.shape(relu_2)[2]*2),
 				method=tf.image.ResizeMethod.NEAREST_NEIGHBOR,
 			)
-			running_output = pool
+			running_output = upsample
 
 		final_conv_1 = tf.layers.conv2d(
 			inputs=running_output,
@@ -163,7 +203,8 @@ class Decoder(BaseModel):
 			strides=(1, 1),
 			padding='same',
 			activation=None,
-			name='conv{}_2'.format(i),
+			# kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+			name='conv_final_1',
 		)
 		final_relu = tf.nn.leaky_relu(
 			features=final_conv_1,
@@ -176,19 +217,20 @@ class Decoder(BaseModel):
 			strides=(1, 1),
 			padding='same',
 			activation=None,
-			name='conv{}_2'.format(i),
+			# kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+			name='conv_final_2',
 		)
-
 		self.outputs = final_conv_2
+		# self.outputs = tf.sigmoid(final_conv_2)
 
 class Critic(BaseModel):
 
-	def __init__(self, inputs, name='encoder'):
+	def __init__(self, inputs, name='critic'):
 		self.inputs = inputs
 		self.name = name
-		with tf.variable_scope(self.name):
+		with tf.variable_scope(self.name, reuse=tf.AUTO_REUSE):
 			self.build_model()
-			variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, self.name)
+			variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, '/'.join(['acai', self.name]))
 			self.saver = tf.train.Saver(var_list=variables, max_to_keep=3)
 
 	def build_model(self):
@@ -202,6 +244,7 @@ class Critic(BaseModel):
 				strides=(1, 1),
 				padding='same',
 				activation=None,
+				# kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
 				name='conv{}_1'.format(i),
 			)
 			relu_1 = tf.nn.leaky_relu(
@@ -215,16 +258,13 @@ class Critic(BaseModel):
 				strides=(1, 1),
 				padding='same',
 				activation=None,
+				# kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
 				name='conv{}_2'.format(i),
 			)
-			# No Leaky ReLU for last layer
-			if i == len(n_filters) - 1:
-				relu_2 = conv_2
-			else:
-				relu_2 = tf.nn.leaky_relu(
-					features=conv_2,
-					alpha=0.2,
-				)
+			relu_2 = tf.nn.leaky_relu(
+				features=conv_2,
+				alpha=0.2,
+			)
 			pool = tf.layers.average_pooling2d(
 				inputs=relu_2,
 				pool_size=(2, 2),
@@ -232,5 +272,32 @@ class Critic(BaseModel):
 				padding='valid'
 			)
 			running_output = pool
+
+		final_conv_1 = tf.layers.conv2d(
+			inputs=running_output,
+			filters=1,
+			kernel_size=(3, 3),
+			strides=(1, 1),
+			padding='same',
+			activation=None,
+			# kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+			name='conv_final_1',
+		)
+		final_relu = tf.nn.leaky_relu(
+			features=final_conv_1,
+			alpha=0.2,
+		)
+		final_conv_2 = tf.layers.conv2d(
+			inputs=final_relu,
+			filters=1,
+			kernel_size=(3, 3),
+			strides=(1, 1),
+			padding='same',
+			activation=None,
+			# kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+			name='conv_final_2',
+		)
+		running_output = tf.sigmoid(final_conv_2)
 		self.outputs = tf.reduce_mean(running_output, axis=[1, 2, 3])
+		self.outputs *= 0.5
 
